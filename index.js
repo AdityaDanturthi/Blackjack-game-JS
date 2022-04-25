c65e0c4789ab8bf2a829f512af020a69c3cc8636
Aditya Danturthi <aditya2096@gmail.com>
2022-04-24 22:02:03 -0230

    button styling

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 function startGame() {
+let firstCard = 10
+let secondCard = 12
+    
+let sum = firstCard + secondCard
 
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+}
+else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack"
+    hasBlackJack = true
+}
+else {
+    message = "You're out of the game!"
+    isAlive = false
+}
 }
