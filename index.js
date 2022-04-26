683dd6def0734701ecdb151e817022b70b649409
Aditya Danturthi <aditya2096@gmail.com>
2022-04-25 22:02:07 -0230

    startGame()

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
+function startGame() {
+    sumEl.textContent = "Sum: " +sum;
 
 if (sum <= 20) {
     message = "Do you want to draw a new card?"
@@ -20,5 +24,7 @@ else {
     message = "You're out of the game!"
     isAlive = false
 }
-    messageEl.textContent = message;
+    
+}
+messageEl.textContent = message;
 }
